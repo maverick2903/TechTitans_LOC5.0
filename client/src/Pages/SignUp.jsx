a951456fb3c52144aba21f350f29742957ad695b
Varun Viswanath <varunvis2903@gmail.com>
2023-03-12 04:26:20 +0530

    something

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -131,7 +131,6 @@ export default function SignIn() {
       setProgress(progress + 25);
     }
   };
-
   const dealingWithSignInFormSubmission = async e => {
     e.preventDefault();
     const resp = await fetch("http://localhost:5000/user/newUser", {
@@ -155,12 +154,15 @@ export default function SignIn() {
       if (responseInJSON.message == "Username is not unique") {
         setErrors({ ...errors, username: "This username is taken" });
         setStep(2);
+        setProgress(25)
       } else if (responseInJSON.message == "Email is not unique") {
         setErrors({
           ...errors,
           email: "An account already exists for this email",
         });
         setStep(1);
+        setProgress(0);
+
       }
     }
   };
